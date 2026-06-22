570df7437c53c6a946a5d28b285b1056f6e619ee
Furo <furini31@gmail.com>
2026-06-22 14:15:07 +0200

    feat: tier draft + blocco reroll ultimi 3 slot (normal/hard) v5.5.18

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.16';
+const CACHE = 'decempionz-v5.5.18';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
