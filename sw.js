e0bd0b89fc86864b67e2505617140ca7e5886891
Furo <furini31@gmail.com>
2026-06-23 00:13:14 +0200

    fix: banner draft, HoF sort, cronaca campagna v5.5.26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.25';
+const CACHE = 'decempionz-v5.5.26';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
