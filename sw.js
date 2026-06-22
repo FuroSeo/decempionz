7735a705084a47fb6dc72bbe6a5564e53c7d8f0e
Furo <furini31@gmail.com>
2026-06-22 18:30:46 +0200

    fix: Olimpia jersey color + Copa group/maxKnock v5.5.25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.24';
+const CACHE = 'decempionz-v5.5.25';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
