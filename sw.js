3134e1a7417e064d6d8f0e75f1dfea69f65a6f44
Furo <furini31@gmail.com>
2026-06-23 10:40:36 +0200

    feat: slot penalty difensori + bonus r10 + Zanetti fix v5.5.29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.28';
+const CACHE = 'decempionz-v5.5.29';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
