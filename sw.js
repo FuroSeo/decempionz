a4d51c2b8b16377fa5d37ace8b758b5884278993
Furo <furini31@gmail.com>
2026-06-25 10:30:13 +0200

    feat: aggiorna conteggi 69 UCL / 40 Copa + card squadre in ucl.html copa.html index.html v5.5.36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.35';
+const CACHE = 'decempionz-v5.5.36';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
