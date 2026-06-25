7d1a5edcf9fc1e61fcc1174dededd2b173c47603
Furo <furini31@gmail.com>
2026-06-25 15:20:43 +0200

    feat: formazione visibile in header draft + pulsante home risultato v5.5.39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.38';
+const CACHE = 'decempionz-v5.5.39';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
