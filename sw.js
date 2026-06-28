b1a9c33f0769780369898d6fa8254ab9a768ef63
Furo <furini31@gmail.com>
2026-06-28 11:54:35 +0200

    feat: local stats (dcz_stats) + blind draft mode v5.5.57

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.56';
+const CACHE = 'decempionz-v5.5.57';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
