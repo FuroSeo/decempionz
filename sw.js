98d3e5270f7ee87329ba69cee7761bf7f3a5f1ab
Furo <furini31@gmail.com>
2026-06-28 16:59:18 +0200

    feat: +17 squads (UCL 74, Copa 46, WC 79) + update all pages v5.5.58

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.57';
+const CACHE = 'decempionz-v5.5.58';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
