bf80e654c57f9ca6470ba2d2973ce026db022d55
Furo <furini31@gmail.com>
2026-06-29 22:14:18 +0200

    feat: challenge W27, gameplay improvements, nat flags, campaign bracket panel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.6.0';
+const CACHE = 'decempionz-v5.6.1';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
