486cba5c5453090383c44c29067b45aa8861bed8
Furo <furini31@gmail.com>
2026-06-29 20:20:21 +0200

    feat: challenge W27, gameplay improvements, GA4 events, i18n fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.5.61';
+const CACHE = 'decempionz-v5.6.0';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
