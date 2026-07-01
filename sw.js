d268c3f2a177583c54b27a9bc0a73655b915b67e
Furo <furini31@gmail.com>
2026-07-01 12:28:43 +0200

    feat: Dynasty Mode ÔÇö scegli il tuo club storico, rose Inter 0203 e City 1617 (v5.6.3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.6.2';
+const CACHE = 'decempionz-v5.6.3';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
