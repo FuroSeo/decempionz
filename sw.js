3c0d06652ad4e3804508759ea35c817904f3e495
Furo <furini31@gmail.com>
2026-07-03 17:28:02 +0200

    deploy: 03/07/2026 17:28:02,35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'decempionz-v5.8.1';
+const CACHE = 'decempionz-v5.8.2';
 const ASSETS = ['/', '/index.html', '/og-image.png', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
